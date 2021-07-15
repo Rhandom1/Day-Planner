6670efd609ab4873e863fb98102ae96864810dfd
Rhandom1 <73659834+Rhandom1@users.noreply.github.com>
2021-07-15 15:12:10 -0500

    Added static rows and linked with moment
    Need to parse the time across all the rows

--- Assets/script.js
+++ Assets/script.js
@@ -1,15 +1,25 @@
 var saveButton = $('.saveBtn');
+var hourEl = $('.hour');
+
 
 //Detects when the page is ready for the javascript
 $(document).ready(function() {
     //Current date displayed at top
     var currentDate = moment().format('MMM Do, YYYY');
     $('#currentDay').text(currentDate);
+
+    // Apply current time to rows
+    var currentTime = moment().format('h a');
+    $('.hour').text(currentTime);
+
+    //Click the save button to save to local storage
+    
+
 })
-// Create calendar rows dynamically
+
+
 //time block rows are color coded for past, present, and future
 //Click on the time block to add and event
-//Click the save button to save to local storage
+
 //Saved events persist after page reload
 
-var saveButton = $('.saveBtn');
