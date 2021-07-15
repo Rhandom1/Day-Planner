bc5ef0067cae8f5e622ae3c9f8e7dc31de8073d0
Rhandom1 <73659834+Rhandom1@users.noreply.github.com>
2021-07-14 23:39:46 -0500

    Current Date functional

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,11 @@
-//Current date displayed at top
-var date = moment().format('MMM Do, YYYY');
+var saveButton = $('.saveBtn');
+
+//Detects when the page is ready for the javascript
+$(document).ready(function() {
+    //Current date displayed at top
+    var currentDate = moment().format('MMM Do, YYYY');
+    $('#currentDay').text(currentDate);
+})
 // Create calendar rows dynamically
 //time block rows are color coded for past, present, and future
 //Click on the time block to add and event
