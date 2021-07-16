96a32f2835c5ec242d425031ed88d5a0d41fa31b
Rhandom1 <73659834+Rhandom1@users.noreply.github.com>
2021-07-16 13:39:37 -0500

    Final coding push

--- Assets/script.js
+++ Assets/script.js
@@ -8,28 +8,42 @@ $(document).ready(function() {
     var currentDate = moment().format('dddd MMM Do, YYYY');
     $('#currentDay').text(currentDate);
 
-    // Apply current time to rows
-    var currentTime = moment().format('h a');
-        $('.hour').text(currentTime);
+    var currentHour = moment().hours();
 
-    function hourTracker(){
-        
-        
+    $('.time-block').each(function(){
+        var id = $(this).attr('id');
+        var getId = localStorage.getItem(id);
+        console.log(getId);
+        //this is the .time-block class
+        //grab t
+        $(this).children('.schedule').val(getId);
 
-    }
+        if (id > currentHour) {
+            $(this).addClass('future')
+        }
+        else if (id < currentHour) {
+            $(this).addClass("past");
+        }
+        else {
+            $(this).addClass('present');
+        }
+    })
 
-    //Click the save button to save to local storage
-    $('#hour1 textarea').val(localStorage.getItem('hour1'));
-    $('#hour2 textarea').val(localStorage.getItem('hour2'));
-    $('#hour3 textarea').val(localStorage.getItem('hour3'));
-    $('#hour4 textarea').val(localStorage.getItem('hour4'));
-    $('#hour5 textarea').val(localStorage.getItem('hour5'));
-    $('#hour6 textarea').val(localStorage.getItem('hour6'));
-    $('#hour7 textarea').val(localStorage.getItem('hour7'));
-    $('#hour8 textarea').val(localStorage.getItem('hour8'));
-    $('#hour9 textarea').val(localStorage.getItem('hour9'));
+    $(".saveBtn").click(function(){
+        //this is the .saveBtn class
+        //Get the value of the sibling of the schedule class and make it the value of value
+        var value = $(this).siblings('.schedule').val();
+        //grabbed the id from the parent element
+        var parentValue = $(this).parent().attr('id');
+        //Saved to localStorage
+        localStorage.setItem(parentValue, value)
+        console.log(value);
+        console.log(parentValue);
+    })
 
 })
+
+
 
 
 //time block rows are color coded for past, present, and future
